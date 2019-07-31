62fef2d747fad6be82ca183d30b43dcd62a143a2
bulat <bulat.nasrulin@gmail.com>
2019-07-31 17:09:43 +0200

    Add noodles draft

--- ledger.py
+++ ledger.py
@@ -1,5 +1,9 @@
 class Ledger:
 
+    def __init__(self):
+        self.is_malicious = False
+        self.overlay_neighbours = {}
+
     def sharable_packet(self):
         pass
 
@@ -9,20 +13,26 @@ class Ledger:
     def get_tx(self, hash):
         pass
 
+    def get_current_balance(self, peer):
+        pass
+
+    def set_malicious(self):
+        self.is_malicious = True
+
 
 class Transaction:
 
-    def __init__(self):
-        self.payload = ""
-        self.source = 12
-        self.target = 13
-        self.sign = ""
-        self.hash = ""
+    def __init__(self, source, target, sign, hash, links, counter, type="source", payload="ab12"):
+        self.payload = payload
+        self.source = source
+        self.target = target
+        self.sign = sign
+        self.hash = hash
         # Last known state/coming after semantics
-        self.links = []
+        self.links = links
         # The transaction place in the ledger/inclusion proof/synchronization help
-        self.counter = 0
-        self.type = "source"
+        self.counter = counter
+        self.type = type
 
 
 class ChainLedger(Ledger):
@@ -32,42 +42,49 @@ class ChainLedger(Ledger):
     """
 
     def __init__(self):
+        super().__init__()
         self.chain = {}
         self.double_spends = {}
         self.last_counter = 0
         self.last_hash = 0
 
         self.in_counter = 0
-        self.in_last_hash = 0
 
     def store_transaction(self, tx: Transaction):
         # check integral consistency
-        if self.chain[tx.counter].hash == tx.hash:
-            # Transaction already in the set
-            return None
-        else:
-            # Double spend detected
-            if tx.counter not in self.double_spends.keys():
-                self.double_spends[tx.counter] = set()
-                self.double_spends[tx.counter].add(self.chain[tx.counter])
-            self.double_spends[tx.counter].add(tx)
-            print("Double spend detected ")
-            if tx.links[0] != self.chain[tx.counter].links[0]:
-                # Two transaction are pointing to different transactions
-                print("Inconsistency detected")
 
-        if self.last_counter == tx.counter and tx.links[0] == self.last_hash:
+        if tx.counter in self.chain.keys():
+            # Transaction with this tx counter is already recorded
+            if self.chain[tx.counter].hash == tx.hash:
+                # The same transaction is already in the set
+                return None
+            else:
+                # Double spend detected
+                if tx.counter not in self.double_spends.keys():
+                    self.double_spends[tx.counter] = set()
+                    self.double_spends[tx.counter].add(self.chain[tx.counter])
+                self.double_spends[tx.counter].add(tx)
+                print("Double spend detected ")
+                if tx.links[0] != self.chain[tx.counter].links[0]:
+                    # Two transaction are pointing to different transactions
+                    # There is a deeper fork, we need to search for
+                    print("Inconsistency detected")
+                    return tx.counter - 1, tx.counter
+
+        elif self.last_counter == tx.counter and tx.links[0] == self.last_hash:
             # Consistent insert
             self.chain[tx.counter] = tx
             self.last_counter = max(self.last_counter, tx.counter)
             self.last_hash = tx.hash
+            if self.last_counter + 1 == self.in_counter:
+                self.last_counter = self.in_counter
+                self.last_hash = self.chain[self.last_counter].hash
+            return None
         else:
             # Inconsistent transaction
             self.chain[tx.counter] = tx
-            self.in_counter
-            # Ask for other transactions
-
-            #
+            self.in_counter = tx.counter
+            return self.last_counter, tx.counter
 
     def is_consistent(self):
         last_hash = 0
@@ -80,6 +97,17 @@ class ChainLedger(Ledger):
             else:
                 return False
         return True
+
+
+class MergeSet(Ledger):
+
+    def __init__(self, neighbours: dict):
+        super().__init__()
+        self.tx_set = set()
+        self.overlay_neighbours = neighbours
+
+    def store_transaction(self, tx):
+        self.tx_set.add(tx)
 
 
 class VectorClock(Ledger):
---
+++ peer.py
@@ -0,0 +1,34 @@
+from noodles.ledger import Ledger, Transaction
+from networkx import Graph
+
+
+class Peer:
+
+    def __init__(self, ledger: Ledger, witness_strategy, init_network: Graph, max_risk=0.1):
+        # storage layer
+        self.ledger = ledger
+        # estimated topology network
+        self.network = init_network
+
+        # Estimate network convergence rate
+
+        # Network has weight between peer?
+
+        # T-consistency: What is the t when peer will be consistent
+
+    def receive_tx(self):
+        pass
+
+    def get_witnesses(self, tx: Transaction):
+        """
+        Witness selection: based on the transaction select peer that would minimize double spend risk. Possible
+        scenarios:
+        1. You receive a transaction from peer P1, estimate the risk and choose witnesses that would
+        minimize the risk of successful double-spend;
+        Send confirmation transaction after estimating the risk.
+        2. When you receive double-signed transaction you update your local ledger and gossip further if needed.
+        3. When you receive one-signed transaction you included as possibly unconfirmed and gossip further if needed.
+        :param tx:
+        :return:
+        """
+        pass
